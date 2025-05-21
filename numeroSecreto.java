02b192855f1ae034012fe61ad446c56ec0e40daf
GustavoGraciano08 <ggraciano.sesi08@gmail.com>
2025-05-21 11:31:20 -0300

    numeroSecreto.java

--- numeroSecreto.java
+++ numeroSecreto.java
@@ -1,5 +1,33 @@
 package desafios;
 
 public class numeroSecreto {
+  public static void main(String[] args) {
+		
+		  Scanner scanner = new Scanner(System.in);
+	        Random rand = new Random();
 
+	        int numeroSecreto = rand.nextInt(100) + 1; 
+	        int tentativas = 0;
+	        int palpite;
+
+	        System.out.println("Vamos jogar! Tente adivinhar o número entre 1 e 100.");
+
+	        while (true) {
+	            System.out.print("Digite seu palpite: ");
+	            palpite = scanner.nextInt();
+	            tentativas++;
+
+	            if (palpite == numeroSecreto) {
+	                System.out.println("Parabéns! Você acertou o número em " + tentativas + " tentativas.");
+	                break;
+	            } else if (palpite < numeroSecreto) {
+	                System.out.println("O número secreto é maior.");
+	            } else {
+	                System.out.println("O número secreto é menor.");
+	            }
+	        }
+
+	        scanner.close();
+
+}
 }
